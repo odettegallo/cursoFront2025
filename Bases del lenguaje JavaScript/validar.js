e3a26d72d6a81b1181d1e4416c6b4afa80c7c827
Odette <odette.gallo@hotmail.com>
2025-06-15 17:00:06 -0400

    peoresna

--- Bases del lenguaje JavaScript/validar.js
+++ Bases del lenguaje JavaScript/validar.js
@@ -1,17 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("formulario");
     form.addEventListener("submit", function(event) {
-        const errorNombre = document.getElementById("errorName");
-        const errorEmail = document.getElementById("errorEmail");
-        const errorTelefono = document.getElementById("errorPhone");
-        const errorMensaje = document.getElementById("errorMessage");
-        // Limpiar mensajes de error previos
-        errorNombre.textContent = "El nombre debe contener mínimo 3 letras";
-        errorEmail.textContent = "El email debe ser válido.";
-        errorTelefono.textContent = "El teléfono debe contener 9 dígitos.";
-        errorMensaje.textContent = "El mensaje debe tener al menos 10 caracteres.";
-        // Validar el formulario
-
+        
         if (!validar()) {
             event.preventDefault(); // Evita el envío del formulario si la validación falla
         }
@@ -19,12 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function validar() {
+let valido = true;
+
     const nombre = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const telefono = document.getElementById("phone").value;
     const mensaje = document.getElementById("message").value;
 
-    const nombreInput = document.getElementById("name");
+
+const nombreInput = document.getElementById("name");
 nombreInput.addEventListener("input", function() {
     const errorNombre = document.getElementById("errorName");
     if (this.value.length < 3 || !/^[a-zA-Z\s]+$/.test(this.value)) {
@@ -67,8 +60,17 @@ mensajeInput.addEventListener("input", function() {
     }
 });
 
+        const errorNombre = document.getElementById("errorName");
+        const errorEmail = document.getElementById("errorEmail");
+        const errorTelefono = document.getElementById("errorPhone");
+        const errorMensaje = document.getElementById("errorMessage");
+        // Limpiar mensajes de error previos
+        errorNombre.textContent = "El nombre debe contener mínimo 3 letras.";
+        errorEmail.textContent = "El email debe ser válido.";
+        errorTelefono.textContent = "El teléfono debe contener 9 dígitos.";
+        errorMensaje.textContent = "El mensaje debe tener al menos 10 caracteres.";
 
-    if (nombre === "" || email === "" || telefono === "" || mensaje === "") {
+        if (nombre === "" || email === "" || telefono === "" || mensaje === "") {
         alert("Por favor, completa todos los campos.");
         return false;
     }
@@ -98,6 +100,9 @@ mensajeInput.addEventListener("input", function() {
     }
 
     return true;
+
+
+
 }
     
 
